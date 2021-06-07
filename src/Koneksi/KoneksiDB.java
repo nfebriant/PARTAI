6c859748c911a8af6194ecfba455dfc4e316dd23
Haves17 <62881325+Haves17@users.noreply.github.com>
2021-06-07 13:46:46 +0700

    Add files via upload

--- src/Koneksi/KoneksiDB.java
+++ src/Koneksi/KoneksiDB.java
@@ -3,31 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Koneksi;
+
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author D
+ * @author Sensei
  */
 public class KoneksiDB {
     static Connection con;
     
     public static Connection connection(){
-        if (con == null){
-            MysqlDataSource data = new MysqlDataSource();
-            data.setPort(8899);
-            data.setDatabaseName("db_pbo");
-            data.setUser("root");
-            data.setPassword("");
-            
-            try {
-                con = (Connection) data.getConnection();
-            } catch (SQLException ex){
-//                logger.getLogger(Koneksi108.Class.getName()).log(level.SEVERE, null, ex);
-                ex.printStackTrace();
-            }
-        }
-        return con;
+         if(con == null){
+             MysqlDataSource data = new MysqlDataSource();
+             data.setDatabaseName("db_tani");
+             data.setUser("root");
+             data.setPassword("");
+             
+             try {
+                 con = (Connection) data.getConnection();
+             } catch (SQLException ex) {
+                 Logger.getLogger(KoneksiDB.class.getName()).log(Level.SEVERE, null, ex);
+             }
+         }
+         return con;
+         
     }
 }
