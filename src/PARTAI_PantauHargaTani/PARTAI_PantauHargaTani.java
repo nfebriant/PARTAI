26ad688bf5034274c307b759f0d06e9b4244c0ab
nfebriant <111201912144@mhs.dinus.ac.id>
2021-06-21 19:55:47 +0700

    FINISHING

---
+++ src/PARTAI_PantauHargaTani/PARTAI_PantauHargaTani.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PARTAI_PantauHargaTani;
+
+import View.FormPartai;
+/**
+ *
+ * @author nanan
+ */
+public class PARTAI_PantauHargaTani {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FormPartai ().setVisible(true);
+    }
+}
